ca541284bb491a1566b036a9e79dc27b4a0c7c1d
geonung Cheong <gwjung@vdotdo.com>
2019-09-16 20:57:25 +0900

    EditText Change

---
+++ app/src/main/java/com/example/galleryapp/HashTagAdapter.java
@@ -0,0 +1,70 @@
+package com.example.galleryapp;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HashTagAdapter extends RecyclerView.Adapter<HashTagAdapter.ViewHolder>{
+    private ArrayList<String> mDataset;
+    Context mContext;
+
+    public HashTagAdapter(Context context, ArrayList<String> Dataset){
+        this.mContext= context;
+        this.mDataset = Dataset;
+    }
+
+    @NonNull
+    @Override
+    public HashTagAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.searchtag_child,null);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        final String hashTag = mDataset.get(position);
+        Log.d("AGAG",""+hashTag);
+        holder.hashtag.setText(hashTag);
+        holder.hashtag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mDataset.remove(position);
+                notifyItemRemoved(position);
+                notifyItemRangeChanged(position,mDataset.size());
+
+            }
+        });
+
+    }
+
+    public void addItem(String hashtag){
+        mDataset.add(hashtag);
+        notifyItemInserted(mDataset.size());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        protected TextView hashtag;
+
+        public ViewHolder(View itemView){
+            super(itemView);
+            this.hashtag = itemView.findViewById(R.id.hashtag);
+
+
+        }
+    }
+}
--- app/src/main/java/com/example/galleryapp/MainActivity.java
+++ app/src/main/java/com/example/galleryapp/MainActivity.java
@@ -1,41 +1,47 @@
 package com.example.galleryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.text.Layout;
 import android.util.Log;
 import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnKeyListener;
-import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.galleryapp.Util.ClearEditText;
 
-import static android.view.KeyEvent.KEYCODE_ENTER;
-import static android.view.KeyEvent.KEYCODE_NUMPAD_0;
-import static android.view.KeyEvent.KEYCODE_SPACE;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity{
-    ClearEditText clearEditText;
-    Layout mainLayout;
-    EditText edit;
+    private RecyclerView RV_HashTagList;
+    private HashTagAdapter hashTagAdapter;
+    private EditText ET_SearchHashTag;
+    ArrayList<String> AL_HashTagList = new ArrayList<>();
+    int a = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        edit = (EditText)findViewById(R.id.edittext);
-        edit.setImeOptions(EditorInfo.IME_ACTION_SEARCH);
-        edit.setOnEditorActionListener(new TextView.OnEditorActionListener()
+
+
+        ET_SearchHashTag = findViewById(R.id.edittext);
+        RV_HashTagList = findViewById(R.id.hashtaglist);
+
+        hashTagAdapter = new HashTagAdapter(getApplicationContext(),AL_HashTagList);
+        RV_HashTagList.setAdapter(hashTagAdapter);
+
+
+
+        RV_HashTagList.setLayoutManager(new GridLayoutManager(getApplicationContext(),6));
+
+        ET_SearchHashTag.setImeOptions(EditorInfo.IME_ACTION_SEARCH);
+        ET_SearchHashTag.setOnEditorActionListener(new TextView.OnEditorActionListener()
         {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event)
@@ -43,12 +49,20 @@ public class MainActivity extends AppCompatActivity{
                 if(actionId == EditorInfo.IME_ACTION_SEARCH)
                 {
                     Toast.makeText(getApplicationContext(),"로그인 성공",Toast.LENGTH_SHORT).show();
-                    edit.setText(null);
+                    AL_HashTagList.add(ET_SearchHashTag.getText().toString());
+                    a++;
+                    hashTagAdapter.notifyDataSetChanged();
+                    Log.d("AGAG",""+AL_HashTagList);
+
+                    ET_SearchHashTag.setText(null);
                     return true;
                 }
                 return false;
             }
         });
+
+
+
     }
 
 
